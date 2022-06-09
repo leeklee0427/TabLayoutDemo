514596f78a319ede135659968456d039fa69a97d
leeklee0427 <billlee0427@126.com>
2022-06-08 21:51:55 -0400

    Initial commit

---
+++ app/src/main/java/com/dli46/tablayout/MainActivity.java
@@ -0,0 +1,35 @@
+package com.dli46.tablayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        adapter.addFragment(new fragment1(), "1");
+        adapter.addFragment(new fragment2(), "2");
+        adapter.addFragment(new fragment3(), "3");
+        viewPager.setAdapter(adapter);
+    }
+
+}
